c262b92e154743a749fe8645de3b95b589691094
Jonathan Carr <rainercarr@gmail.com>
2020-06-01 14:14:13 -0500

    Design changes to Schema, started work on Models

--- livestock_records/models.py
+++ livestock_records/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Species(models.Model):
+    species_name = models.CharField(max_length=30)
+
+
+class Animal(models.Model):
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    color = models.ForeignKey(Color)
+    dam = models.ForeignKey(self)
+    sire = models.ForeignKey(self)
+    
+
+class NoteReason(models.Model):
+    reason = models.CharField(max_length=30)
+
+class Note(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    date = models.DateField()
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ To install/run this app you need to run the following:
 virtualenv farm_records
 .\Scripts\activate
 
-pip install --default_timeout=100 django==3.0.6
-mkdir farm_records_src 
-cd farm_records_src
-git clone (this repository)
+pip install --default_timeout=100 django==3.0.6 \n
+mkdir farm_records_src \n
+cd farm_records_src \n
+git clone (this repository) \n
